62470cfb306166af3f4ee156efbe132a3842d6e1
Owen Williams <owenm.williams@gmail.com>
2020-10-16 22:40:54 -0400

    First commit on model branch

--- sql/qry - Fn_Lookup.sql
+++ sql/qry - Fn_Lookup.sql
@@ -9,6 +9,6 @@
 -- COMMIT;
 
  BEGIN;
- SELECT lookup_YrCostPopShareAirPark('mycur', 2018::varchar, 100000::integer, 30000::integer, 0.2::decimal, 1.5::decimal, 2.0::decimal, 4::integer);
+ SELECT lookup_YrCostPopShareAirPark('mycur', 2018::varchar, 100000::integer, 20000::integer, 0.2::decimal, 1.5::decimal, 2.5::decimal, 5::integer);
  FETCH ALL IN "mycur";
  COMMIT;
