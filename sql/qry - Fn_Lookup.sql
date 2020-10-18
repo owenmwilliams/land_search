e96a68750248e59511b2b7961c8d2a6bfc357d39
Owen Williams <owenm.williams@gmail.com>
2020-10-17 21:13:50 -0400

    First draft of estimation function and populating value estimates for population table

--- sql/qry - Fn_Lookup.sql
+++ sql/qry - Fn_Lookup.sql
@@ -8,7 +8,12 @@
 -- FETCH ALL IN "mycur";
 -- COMMIT;
 
+-- BEGIN;
+-- SELECT lookup_YrCostPopShareAirPark('mycur', 2018::varchar, 100000::integer, 20000::integer, 0.2::decimal, 1.5::decimal, 2.5::decimal, 5::integer);
+-- FETCH ALL IN "mycur";
+-- COMMIT;
+ 
  BEGIN;
- SELECT lookup_YrCostPopShareAirPark('mycur', 2018::varchar, 100000::integer, 20000::integer, 0.2::decimal, 1.5::decimal, 2.5::decimal, 5::integer);
+ SELECT est_LandValue('mycur', 3.0, 0.2, 5);
  FETCH ALL IN "mycur";
  COMMIT;
