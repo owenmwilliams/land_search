fe86535a6582cee7b6c5a1c39bd65bbb9e089860
Owen Williams <owenm.williams@gmail.com>
2020-10-31 08:16:47 -0400

    ...

--- models/dtl/api/dtl_parks.py
+++ models/dtl/api/dtl_parks.py
@@ -4,7 +4,7 @@ import requests, json, pandas as pd
 
 def parks_get():
     endpoint = "https://developer.nps.gov/api/v1/parks"
-    HEADERS = {"X-Api-Key": "<INSERT KEY HERE"}
+    HEADERS = {"X-Api-Key": "<INSERT KEY HERE>"}
     params = {"limit":"500"}
     response = requests.get(endpoint, params = params, headers=HEADERS)
     d = response.json()
