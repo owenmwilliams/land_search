f9aa0771a012b169a863ad34579e7cca4ef4e7d3
Owen Williams <owenm.williams@gmail.com>
2020-11-03 19:38:15 -0500

    Passing API key for as env variable for parks get

--- models/dtl/api/dtl_parks.py
+++ models/dtl/api/dtl_parks.py
@@ -1,10 +1,13 @@
 import requests, json, pandas as pd
+import os
+from dotenv import load_dotenv
 
 # Returns parks (lat, long) from National Park Service API
 
 def parks_get():
+    load_dotenv()
     endpoint = "https://developer.nps.gov/api/v1/parks"
-    HEADERS = {"X-Api-Key": "<INSERT KEY HERE>"}
+    HEADERS = {"X-Api-Key": os.getenv("DATAGOV_KEY")}
     params = {"limit":"500"}
     response = requests.get(endpoint, params = params, headers=HEADERS)
     d = response.json()
