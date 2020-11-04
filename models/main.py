9cf1290278f56d0c7120729983ef9d9c8affce0d
Owen Williams <owenm.williams@gmail.com>
2020-11-03 21:36:29 -0500

    Updating for full census data set pull

--- models/main.py
+++ models/main.py
@@ -1,15 +1,20 @@
-from dtl.wrt.census_JSON_store import wrt_temp
+from dtl.api.dtl_census_copy import census_get
 from dtl.wrt.pop_table import wrt_pop
 from datetime import datetime
 from est.db.cur import con_cur
 import psycopg2
 from psycopg2 import sql
+import pandas
+import os
 
 StartWhile = datetime.now()
 print('*********************************', '\n')
 
-#wrt_census("TestTableName")
-wrt_pop("onlyuselowercase")
+wrt_pop("dat_full_census")
+
+# x = census_get()
+# print(x)
+# print(len(x))
 
 EndWhile = datetime.now()
 print('*********************************', '\n', 'Time for calculation: ', EndWhile - StartWhile)
