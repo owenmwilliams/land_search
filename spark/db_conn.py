12edda6308f7c335ea50cf847033a8c552df3604
Owen Williams <owenm.williams@gmail.com>
2020-11-27 16:04:17 -0800

    Changing spark db connect to test on ls-cluster

--- spark/db_conn.py
+++ spark/db_conn.py
@@ -8,18 +8,17 @@ print('*********************************', '\n')
 spark = SparkSession \
         .builder \
         .appName("dumb_nothingness") \
-        .config("spark.jars", "/Library/Java/Extensions/pgsql_driver.jar") \
+        .config("spark.jars", "/usr/lib/jvm/java-8-openjdk-arm64/jre/lib/ext/postgresql-42.2.18.jar") \
         .getOrCreate()
 
 pgDF = spark.read.format("jdbc")\
     .option("driver", "org.postgresql.Driver")\
-    .option("url", "jdbc:postgresql://localhost:5434/owenwilliams")\
-    .option("dbtable", "full_census")\
+    .option("url", "jdbc:postgresql://pi0:5434/owenwilliams")\
+    .option("dbtable", "county_population_csv")\
     .load()
 
-pgDF.select("name").distinct().show()
-pgDF.select(countDistinct("name")).show()
-pgDF.select(max("pop")).show()
+pgDF.select("state").distinct().show()
+pgDF.select(min("land_value_estimate")).show()
 
 EndWhile = datetime.now()
 print('*********************************', '\n', 'Time for calculation: ', EndWhile - StartWhile)
