da9c906269e08b022629aa6413d3407fb5b1d7c6
Owen Williams <owenm.williams@gmail.com>
2020-11-29 11:02:54 -0800

    Updating core psql tables

--- sql/pgpl - Census Pop View.sql
+++
@@ -1,9 +0,0 @@
- -- Create a view of 2019 population data by county
-CREATE MATERIALIZED VIEW public.county_pop_2019 AS 
-	SELECT DISTINCT state, county, name, pop FROM census_table
-	WHERE race = 0
-	AND sex = 0
-	AND age_group = 0
-	AND hisp = 0
-	AND date_code = 12
-	ORDER BY name;
