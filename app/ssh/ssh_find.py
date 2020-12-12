7730dd0d7be2b75adef7b193595a5c60ac21e6fc
Owen Williams <owenm.williams@gmail.com>
2020-12-12 13:13:33 -0500

    Added ssh command for find_state; returns only couple rows TODO: return full df

--- app/ssh/ssh_find.py
+++ app/ssh/ssh_find.py
@@ -10,15 +10,17 @@ remote_host = 'pi0'
 remote_port = 22
 ssh_key = os.getenv("ssh_key")
   
-client = paramiko.SSHClient()
-client.load_system_host_keys()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(remote_host, port=remote_port, username=remote_user, key_filename=ssh_key)
-transport = client.get_transport()
-channel = client.invoke_shell()
+
 
 
 def sf_lucky():
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(remote_host, port=remote_port, username=remote_user, key_filename=ssh_key)
+    transport = client.get_transport()
+    channel = client.invoke_shell()
+
     stdin = channel.makefile('w')
     stdout = channel.makefile('r')
     stdin.write('''
@@ -35,6 +37,45 @@ def sf_lucky():
             print(line.rstrip())
         elif line.strip().startswith('0  '):
             print(line.rstrip())
+
+    stdout.close()
+    stdin.close()
+    client.close()
+
+def sf_state(state):
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(remote_host, port=remote_port, username=remote_user, key_filename=ssh_key)
+    transport = client.get_transport()
+    channel = client.invoke_shell()
+
+    stdin = channel.makefile('w')
+    stdout = channel.makefile('r')
+    stdin.write("""
+    cd /opt/ls-cluster-v0.0.1/models
+    python3 -c 'import main; x = main.find_state("%s"); print(x)'
+    exit
+    """ % state)
+
+    while True:
+        line = stdout.readline()
+        # print(line.rstrip())
+        if not line:
+            break
+        elif line.strip().startswith('County'):
+            print(line.rstrip())
+        else: 
+            try:
+                if line.strip()[0].isdigit():
+                    if line.strip()[1:3].isspace():
+                        print(line.rstrip())
+            except IndexError:
+                pass
+        # elif line.strip().startswith('0  '):
+        #     print(line.rstrip()[0])
+        #     print(line.rstrip()[1:2])
+        
 
     stdout.close()
     stdin.close()
