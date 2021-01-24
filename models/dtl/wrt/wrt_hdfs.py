2fd45042b4f560b41e3847a1ec0e6e9211a9b861
Owen Williams <owenm.williams@gmail.com>
2021-01-24 14:02:01 -0500

    Condensed hdfs save function

--- models/dtl/wrt/wrt_hdfs.py
+++ models/dtl/wrt/wrt_hdfs.py
@@ -9,44 +9,23 @@ from dotenv import load_dotenv
 
 # Getting input from API into parquet on HDFS
 def to_hdfs(api):
-    if api == "Census-demo":
-        state_list = st_fips_get()
-        today = date.today()
-        path = today.strftime("%Y-%m-%d")
-        for i in range(len(state_list)):
-            record = state_list.iloc[i]
-            state = record['state']
-            file_name = record['NAME']
-            file_name = file_name.rsplit(', ', 1)[1]
-            file_name = file_name.replace(" ", "_")
+    state_list = st_fips_get()
+    today = date.today()
+    path = today.strftime("%Y-%m-%d")
+    for i in range(len(state_list)):
+        record = state_list.iloc[i]
+        state = record['state']
+        file_name = record['NAME']
+        file_name = file_name.rsplit(', ', 1)[1]
+        file_name = file_name.replace(" ", "_")
+        if api == "Census-demo":
             hdfs_save('/ls_raw_dat/census_demo/%s' % path, '%s' % file_name, census_get(state))
-    
-    elif api == "Census-time":
-        state_list = st_fips_get()
-        today = date.today()
-        path = today.strftime("%Y-%m-%d")
-        for i in range(len(state_list)):
-            record = state_list.iloc[i]
-            state = record['state']
-            file_name = record['NAME']
-            file_name = file_name.rsplit(', ', 1)[1]
-            file_name = file_name.replace(" ", "_")        
+        elif api == "Census-time":
             hdfs_save('/ls_raw_dat/census_time/%s' % path, '%s' % file_name, census_time_get(state))
-
-    elif api == "Census-house":
-        state_list = st_fips_get()
-        today = date.today()
-        path = today.strftime("%Y-%m-%d")
-        for i in range(len(state_list)):
-            record = state_list.iloc[i]
-            state = record['state']
-            file_name = record['NAME']
-            file_name = file_name.rsplit(', ', 1)[1]
-            file_name = file_name.replace(" ", "_")        
+        elif api == "Census-house":
             hdfs_save('/ls_raw_dat/census_housing/%s' % path, '%s' % file_name, census_housing_get(state))
-
-    else:
-        print('Other APIs links yet to be built.')
+        else:
+            print('Other APIs links yet to be built.')
 
 def to_local(api):
     if api == "Census-demo":
