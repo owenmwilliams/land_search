91e825977f15642d4d1f84275302df0b7ef2a695
Owen Williams <owenm.williams@gmail.com>
2021-05-17 21:11:52 -0700

    Pulls list of all counties, generates URL, finds number of pagest

--- models/dtl/scp/scp_loa.py
+++ models/dtl/scp/scp_loa.py
@@ -2,16 +2,43 @@ from urllib.request import Request, urlopen
 import ssl
 from bs4 import BeautifulSoup
 import re
+from est.db.cur import con_cur
+import pandas as pd
 
+#Need full set of counties & states
+cur, con = con_cur()
+cur.execute("""
+                SELECT DISTINCT county, usps FROM countylatlong
+            """)
+cty_array = pd.DataFrame(cur.fetchall(), columns = ['County','State'])
+con.close()
+
+#Build url addendum
+cty_list = []
+for i in range(len(cty_array)):
+    if cty_array['State'][i] == 'CA':
+        x = '{0}-{1}'.format(cty_array['County'][i], cty_array['State'][i])
+        x = x.replace(' ', '-')
+        cty_list.append(x)
+print(cty_list)
+
+#Need to loop through URLs - by county / state, then by page
 context = ssl._create_unverified_context()
-url = Request("https://www.landsofamerica.com/Pulaski-County-KY/all-land/no-house/", headers={'User-Agent': 'Mozilla/5.0'})
+url = Request("https://www.landsofamerica.com/Yolo-County-CA/all-land/no-house/", headers={'User-Agent': 'Mozilla/5.0'})
 page = urlopen(url, context=context)
 html = page.read().decode("utf-8")
 soup = BeautifulSoup(html, "html.parser")
-# a, b, c = soup.find_all("span title")
-# print(a, b, c)
-# print(soup.find_all('span'))
 
+#Need to id # of pages for given county
+page_num = []
+for i in soup.find_all('span', {'class': 'd41b7'}):
+    try:
+        page_num.append(int(i.get_text().strip()))
+    except:
+        pass
+print(max(page_num))
+
+#Need to build into array / pDF, set data type, and save to parquet files
 for i in soup.find_all('span', {'class':'_32f8d'}):
     print(i.get_text().strip())
 
@@ -21,5 +48,3 @@ for i in soup.find_all('span', {'class':'_1a278'}):
 for i in soup.find_all("span"):
     if i.get('title') is not None:
         print(i.get('title'))
-
-    # title=re.compile("RAM")).text
