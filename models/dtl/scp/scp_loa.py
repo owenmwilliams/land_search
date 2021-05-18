e98ab6d127f8407875dac48d426d440154f69148
Owen Williams <owenm.williams@gmail.com>
2021-05-17 21:41:56 -0700

    Building in tha loops

--- models/dtl/scp/scp_loa.py
+++ models/dtl/scp/scp_loa.py
@@ -16,35 +16,63 @@ con.close()
 #Build url addendum
 cty_list = []
 for i in range(len(cty_array)):
-    if cty_array['State'][i] == 'CA':
+    if cty_array['State'][i] == 'DE':
         x = '{0}-{1}'.format(cty_array['County'][i], cty_array['State'][i])
         x = x.replace(' ', '-')
         cty_list.append(x)
 print(cty_list)
 
+
+df = pd.DataFrame(columns = ['Price', 'Acreage', 'Location', 'County'])
 #Need to loop through URLs - by county / state, then by page
-context = ssl._create_unverified_context()
-url = Request("https://www.landsofamerica.com/Yolo-County-CA/all-land/no-house/", headers={'User-Agent': 'Mozilla/5.0'})
-page = urlopen(url, context=context)
-html = page.read().decode("utf-8")
-soup = BeautifulSoup(html, "html.parser")
+for j in range(len(cty_list)):
+    context = ssl._create_unverified_context()
+    url = Request("https://www.landsofamerica.com/{0}/all-land/no-house/".format(cty_list[j]), headers={'User-Agent': 'Mozilla/5.0'})
+    page = urlopen(url, context=context)
+    html = page.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
 
-#Need to id # of pages for given county
-page_num = []
-for i in soup.find_all('span', {'class': 'd41b7'}):
-    try:
-        page_num.append(int(i.get_text().strip()))
-    except:
-        pass
-print(max(page_num))
+    for i in soup.find_all('span', {'class':'_32f8d'}):
+        df.at[i, 'Price'] = i.get_text().strip()
 
-#Need to build into array / pDF, set data type, and save to parquet files
-for i in soup.find_all('span', {'class':'_32f8d'}):
-    print(i.get_text().strip())
+    for i in soup.find_all('span', {'class':'_1a278'}):
+        df.at[i, 'Acreage'] = i.get_text().strip()
 
-for i in soup.find_all('span', {'class':'_1a278'}):
-    print(i.get_text().strip())
+    for i in soup.find_all("span"):
+        if i.get('title') is not None:
+            df.at[i, 'Location'] = i.get_text().strip()
 
-for i in soup.find_all("span"):
-    if i.get('title') is not None:
-        print(i.get('title'))
+    #Need to id # of pages for given county
+    page_num = []
+    for i in soup.find_all('span', {'class': 'd41b7'}):
+        try:
+            page_num.append(int(i.get_text().strip()))
+        except:
+            pass
+
+    print(page_num)
+
+    k=2
+    while k <= max(page_num):
+        context = ssl._create_unverified_context()
+        url = Request("https://www.landsofamerica.com/{0}/all-land/no-house/page-{1}".format(cty_list[j], k), headers={'User-Agent': 'Mozilla/5.0'})
+        page = urlopen(url, context=context)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+
+        #Need to build into array / pDF, set data type, and save to parquet files
+        for i in soup.find_all('span', {'class':'_32f8d'}):
+            print(i.get_text().strip())
+
+
+        for i in soup.find_all('span', {'class':'_1a278'}):
+            print(i.get_text().strip())
+
+        for i in soup.find_all("span"):
+            if i.get('title') is not None:
+                print(i.get('title'))
+
+        k=k+1
+
+
+print(df)
